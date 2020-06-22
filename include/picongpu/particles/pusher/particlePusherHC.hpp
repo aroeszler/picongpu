9df9eb0c1a4302fd92a39f63557dde4c4268b423
Roessler <roessl45@hemera5.cluster>
2020-06-22 18:13:45 +0200

    compiles (exclamation mark)

--- include/picongpu/particles/pusher/particlePusherHC.hpp
+++ include/picongpu/particles/pusher/particlePusherHC.hpp
@@ -56,86 +56,64 @@ struct Push
         auto bField  = functorBField(pos);
         auto eField  = functorEField(pos);
 
-        const float_X QoM = charge / mass;
-
         const float_X deltaT = DELTA_T;
 
 
-        Gamma gamma;
+        Gamma gamma;        
         
-        /* ORIGINAL BORIS IMPLEMENTATION
-        const float_X gamma_reci = float_X(1.0) / gamma(mom_minus, mass);
-        const float3_X t = float_X(0.5) * QoM * bField * gamma_reci * deltaT;
-        auto s  = float_X(2.0) * t * (float_X(1.0) / (float_X(1.0) + pmacc::math::abs2(t)));
+        const MomType mom_minus = mom + float_X(0.5) * charge * eField * deltaT;
 
-        const MomType mom_prime = mom_minus + pmacc::math::cross(mom_minus, t);
-        const MomType mom_plus = mom_minus + pmacc::math::cross(mom_prime, s);
+        const sqrt_HC::float_X gamma_minus = gamma( mom_minus , mass );
+        
+        const sqrt_HC::float3_X tau = precisionCast<sqrt_HC::float_X>(float_X(0.5) * bField * charge * deltaT);
 
-        const MomType new_mom = mom_plus + float_X(0.5) * charge * eField * deltaT;
-
+        const sqrt_HC::float_X sigma = pmacc::math::abs2(gamma_minus) - pmacc::math::abs2(tau);
+        
+        const sqrt_HC::float_X u_star = pmacc::math::dot(
+                             precisionCast<sqrt_HC::float_X>(mom_minus), tau
+                        ) / precisionCast<sqrt_HC::float_X>( mass * SPEED_OF_LIGHT );
+       
+        const sqrt_HC::float_X gamma_plus = math::sqrt(( sigma + math::sqrt( pmacc::math::abs2(sigma) + (sqrt_HC::float_X(4.0)) * (pmacc::math::abs2(tau) + pmacc::math::abs2(u_star))))* (sqrt_HC::float_X(0.5)));
+        
+        const sqrt_HC::float3_X t_vector = ((sqrt_HC::float_X(1.0))/gamma_plus) * tau;
+        
+        const sqrt_HC::float_X s = (sqrt_HC::float_X(1.0))/(sqrt_HC::float_X(1.0) + pmacc::math::abs2(t_vector));
+                                    
+        const MomType mom_plus = precisionCast<float_X>(
+                  s * ( precisionCast<sqrt_HC::float_X>( mom_minus ) 
+                          + t_vector * pmacc::math::dot( 
+                              precisionCast<sqrt_HC::float_X>( mom_minus ) 
+                               , t_vector 
+                          )
+                      ) 
+                  + (pmacc::math::cross(
+                   precisionCast<sqrt_HC::float_X>( mom_minus ) , t_vector)));
+                                                        
+         const MomType new_mom = mom_plus + eField * charge * deltaT * float_X(0.5) + 
+                              pmacc::math::cross( mom_plus ,
+                                 precisionCast<float_X>(t_vector));
+         
+        const sqrt_HC::float_X gamma_final = gamma( new_mom, mass);
+                     
         particle[ momentum_ ] = new_mom;
 
         Velocity velocity;
+        
         const float3_X vel = velocity(new_mom, mass);
 
         for(uint32_t d=0;d<simDim;++d)
-        {
-            pos[d] += (vel[d] * deltaT) / cellSize[d];
-        }
-        */
-        
-        // gleich überall in mom umrechnen
-        //wie heißt das "alte" Momentum?
-        
-        //const sqrt_HC::float_X gamma_i = gamma( mom , mass ); //brauchen wir das überhaupt?
-        
-        //const sqrt_HC::float3_X x_first_half = pos[d] + mom * (deltaT * sqrt_HC::float_X(0.5)/(gamma_i * mass));
-        
-        const MomType mom_minus = mom + sqrt_HC::float_X(0.5) * charge * eField * deltaT;
-        
-        const sqrt_HC::float_X gamma_minus = gamma( mom_minus , mass );
-        
-        const sqrt_HC::float3_X tau = (sqrt_HC::float_X(0.5)) * bField * charge * deltaT;
-        
-        const sqrt_HC::float_X sigma = pmacc::math::abs2(gamma_minus) - pmacc::math::abs(tau); //ist die abs2 Fkt sowohl für float_X als auch float3_X passend?
-        
-        const sqrt_HC::float3_X u_star = pmacc::math::dot( (mom_minus * sqrt_HC::float_X(1.0) / mass ) , tau * ((sqrt_HC::float_X(1.0)) / c ));
-        // müssen die alle const sein?
-        
-        const sqrt_HC::float_X gamma_plus = math::sqrt(( sigma + math::sqrt( pmacc::math::abs2(sigma) + (sqrt_HC::float_X(4.0)) * (pmacc::math::abs2(tau) + pmacc::math::abs2(u_star))))* (sqrt_HC::float_X(0.5)));
-        
-        const sqrt_HC::float3_X t = ((sqrt_HC::float_X(1.0))/gamma_plus) * tau;
-        
-        const sqrt_HC::float_X s = (sqrt_HC::float_X(1.0))/(sqrt_HC::float_X(1.0) + pmacc::math::abs2(t));
-                                    
-         const MomType mom_plus = (s * (mom_minus + t_vector * pmacc::math::dot( mom_minus , t_vector )) + (pmacc::math::cross(mom_minus ,t_vector))); //nochmal angucken, sollte man mom_minus/m nehmen und am ende *m nehmen? eher nicht
-                                                        
-         const MomType new_mom = mom_plus + eField * charge * deltaT * (sqrt_HC::float_X(0.5)) + pmacc::math::cross(mom_plus,t_vector);
-        //geht das so einfach?
-         
-        const sqrt_HC::float_X gamma_final = gamma( new_mom, mass); //???
-                     
-         //position update
-         //const sqrt_HC::float3_X x_i = x_first_half + new_mom * deltaT * ((sqrt_HC::float_X(0.5))/ (mass * gamma_final))
-         particle[ momentum_ ] = new_mom;
-
-         Velocity velocity;
-        
-         const float3_X vel = velocity(new_mom, mass);
-
-         for(uint32_t d=0;d<simDim;++d)
             {
-                pos[d] += (vel[d] * deltaT) / cellSize[d];
+               pos[d] += (vel[d] * deltaT) / cellSize[d];
             }
         
         
     }
 
-    /*static pmacc::traits::StringProperty getStringProperties()
+    static pmacc::traits::StringProperty getStringProperties()
     {
-        pmacc::traits::StringProperty propList( "name", "Boris" );
+        pmacc::traits::StringProperty propList( "name", "HC" );
         return propList;
-    }*/
+    }
 };
-} // namespace particlePusherBoris
+} // namespace particlePusherHC
 } // namespace picongpu
